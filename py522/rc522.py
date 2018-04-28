aa77c167ee2ff2c9aa949bec030455649c85252f
Marcos Del Sol Vives <marcos@dracon.es>
2018-04-28 23:53:22 +0200

    Optimize RC522 for success, and cache CRC status

--- py522/rc522.py
+++ py522/rc522.py
@@ -67,6 +67,7 @@ class RC522:
 
 	def __init__(self):
 		self.collision = None
+		self._crc_enabled = None
 
 	def antenna_on(self):
 		self._regwrite(RC522.Reg.TxControl, 0x83)
@@ -89,8 +90,7 @@ class RC522:
 		self.antenna_on()
 
 	def scan(self, wakeup=False):
-		self._regwrite(RC522.Reg.RxMode, 0x00)
-		self._regwrite(RC522.Reg.TxMode, 0x00)
+		self._enable_crc(False)
 
 		try:
 			if wakeup:
@@ -106,8 +106,7 @@ class RC522:
 			anticol = bytearray([0x91 + ct * 2, 0, 0, 0, 0, 0, 0])
 			goodcount = 0
 
-			self._regwrite(RC522.Reg.RxMode, 0x00)
-			self._regwrite(RC522.Reg.TxMode, 0x00)
+			self._enable_crc(False)
 
 			while goodcount < 40:
 				anticol[1] = (2 + goodcount // 8) << 4 | (goodcount % 8)
@@ -148,8 +147,7 @@ class RC522:
 	def select(self, uid):
 		assert(len(uid) in [4, 7, 10])
 
-		self._regwrite(RC522.Reg.RxMode, 0x00)
-		self._regwrite(RC522.Reg.TxMode, 0x00)
+		self._enable_crc(False)
 		self._transceive_bits(b'\x26', 7)
 
 		ct = 1
@@ -168,14 +166,13 @@ class RC522:
 
 			resp = self.transceive(cmd)
 			print(resp)
-			
+
 			ct = ct + 1
 
 	def send(self, request):
 		self._prepare_tx(request)
 
-		self._regwrite(RC522.Reg.TxMode, 0x80)
-		self._regwrite(RC522.Reg.RxMode, 0x80)
+		self._enable_crc(True)
 
 		# Now run command
 		self._run_command(RC522.CmdTransmit)
@@ -185,8 +182,7 @@ class RC522:
 	def transceive(self, request):
 		self._prepare_tx(request)
 
-		self._regwrite(RC522.Reg.TxMode, 0x80)
-		self._regwrite(RC522.Reg.RxMode, 0x80)
+		self._enable_crc(True)
 
 		self._run_command(RC522.CmdTransceive)
 		self._regwrite(RC522.Reg.BitFraming, 0x80)
@@ -205,11 +201,6 @@ class RC522:
 
 		self._regwrite(RC522.Reg.Command, cmd | rcvoff << 5)
 
-	def _write_fifo(self, data):
-		# Clear FIFO
-		self._regwrite(RC522.Reg.FIFOLevel, 0x80)
-		self._regwritebulk(RC522.Reg.FIFOData, data)
-
 	def _read_fifo(self):
 		count = self._regread(RC522.Reg.FIFOLevel) & 0x7F
 		return self._regreadbulk(RC522.Reg.FIFOData, count)
@@ -222,18 +213,26 @@ class RC522:
 		self._regwrite(RC522.Reg.ComIrq, 0x7F)
 
 		# Write data to FIFO
-		self._write_fifo(request)
+		self._regwritebulk(RC522.Reg.FIFOData, request)
 
 	def _wait_rx(self):
 		for x in range(1, 5):
 			if self._regread(RC522.Reg.ComIrq) & 0x20:
 				return
+
+		# Clear FIFO reg
+		self._regwrite(RC522.Reg.FIFOLevel, 0x80)
+
 		raise Exception("RX timeout")
 
 	def _wait_tx(self):
 		for x in range(1, 5):
 			if self._regread(RC522.Reg.ComIrq) & 0x40:
 				return
+
+		# Clear FIFO reg
+		self._regwrite(RC522.Reg.FIFOLevel, 0x80)
+
 		raise Exception("TX timeout")
 
 	def _transceive_bits(self, request, bitlen=None, rxalign=0):
@@ -262,3 +261,13 @@ class RC522:
 			col = (col & 0x1F) - 1
 
 		return (self._read_fifo(), col)
+
+	def _enable_crc(self, enabled):
+		if enabled != self._crc_enabled:
+			if enabled:
+				self._regwrite(RC522.Reg.RxMode, 0x80)
+				self._regwrite(RC522.Reg.TxMode, 0x80)
+			else:
+				self._regwrite(RC522.Reg.RxMode, 0x00)
+				self._regwrite(RC522.Reg.TxMode, 0x00)
+			self._crc_enabled = enabled
